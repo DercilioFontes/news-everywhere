affed39723925e101ac57383656217903f386985
DercilioFontes <dercilioafontes@gmail.com>
2018-09-14 22:42:25 -0700

    Created a reduced version of the data for testing and organized data in an object.

--- app.js
+++ app.js
@@ -5,28 +5,49 @@ const csvParse = require('csv-parse');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+// Object to organize the data
+const organizedDataObject = {};
+
 // Reading CSV file
-fs.createReadStream('data.csv')
+fs.createReadStream('data-reduced.csv')
   .pipe(csvParse())
-  .on('data', (data) => {
+  .on('data', (row) => {
     try {
       // Process row
-      console.log(data);
+      // console.log(row);
+      // Checks if user ID is in the organizedDataObject
+      if(organizedDataObject[row[1]]) {
+        // Updates organizedDataObject
+        organizedDataObject[row[1]].count++;
+        organizedDataObject[row[1]].os.push(row[2]);
+        organizedDataObject[row[1]].device.push(row[3]);
+        organizedDataObject[row[1]].datetime.push(row[0]);
+      } else {
+        // Or creates a new user
+        organizedDataObject[row[1]] = {
+          id: row[1],
+          count: 1,
+          os: [row[2]],
+          device: [row[3]],
+          datetime: [row[0]],
+        };
+      }
     } catch(error) {
       console.log(error.message);
     }
   })
   .on('end', () => {
     console.log("Finished");
+    console.log(organizedDataObject);
   });
 
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World\n');
+// });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
