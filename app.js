61a1acb35be38854fe4d797072f63b0991803815
DercilioFontes <dercilioafontes@gmail.com>
2018-09-14 17:01:04 -0700

    Installed csv-parse and reading the csv file.

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-import { createServer } from 'http';
+const http = require('http');
+const fs = require('fs');
+const csvParse = require('csv-parse');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = createServer((req, res) => {
+// Reading CSV file
+fs.createReadStream('data.csv')
+  .pipe(csvParse())
+  .on('data', (data) => {
+    try {
+      // Process row
+      console.log(data);
+    } catch(error) {
+      console.log(error.message);
+    }
+  })
+  .on('end', () => {
+    console.log("Finished");
+  });
+
+
+const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
